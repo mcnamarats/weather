35f0fa8f949888fe430d9af73fedecc19911fe31
Tim McNamara <tim.mcnamara@gmail.com>
2018-07-29 11:44:14 +0800

    added weather to Redux module

--- src/__tests__/module.test.js
+++ src/__tests__/module.test.js
@@ -1,6 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import reducer, { actions, types, initialState } from '../module';
+import reducer, { actions, types, initialState, normalize } from '../module';
+import weather from '../mock-data/weather.json';
 
 const mockStore = configureMockStore([thunk]);
 
@@ -49,11 +50,73 @@ describe('Weather Redux module', () => {
       const payload = [{ id: 12, name: 'name' }];
       const expected = {
         cities: [{ value: payload[0].id, text: payload[0].name }],
-        isLoading: false
+        isLoading: false,
+        weather: {}
       };
       expect(
         reducer(initialState, { type: types.FETCH_CITIES_SUCCESS, payload })
       ).toEqual(expected);
+    });
+  });
+
+  describe('Weather actions', () => {
+    test('creates an action for fetch request', () => {
+      const expected = { type: types.FETCH_WEATHER_REQUEST };
+      expect(actions.fetchWeatherRequest()).toEqual(expected);
+    });
+
+    test('creates and action for for fetch success', () => {
+      const payload = 'payload';
+      const expected = {
+        type: types.FETCH_WEATHER_SUCCESS,
+        payload: 'payload'
+      };
+      expect(actions.fetchWeatherSuccess(payload)).toEqual(expected);
+    });
+
+    test('creates FETCH_WEATHER_SUCCESS when finished', () => {
+      const store = mockStore({ weather: {} });
+      const expected = [
+        {
+          type: types.FETCH_WEATHER_REQUEST
+        },
+        {
+          type: types.FETCH_WEATHER_SUCCESS,
+          payload: normalize(weather.list)
+        }
+      ];
+      store.dispatch(actions.fetchWeather('12345')).then(() => {
+        store.getActions()[1].payload = normalize(
+          store.getActions()[1].payload.list
+        );
+        expect(store.getActions()).toEqual(expected);
+      });
+    });
+
+    describe('Weather reducer', () => {
+      test('returns initial state for undefined action', () => {
+        expect(reducer(undefined, {})).toEqual(initialState);
+      });
+
+      test('handles FETCH_WEATHER_REQUEST', () => {
+        expect(
+          reducer(initialState, { type: types.FETCH_WEATHER_REQUEST }).isLoading
+        ).toBeTruthy();
+      });
+
+      test('handles FETCH_WEATHER_SUCCESS', () => {
+        const expected = {
+          cities: [],
+          isLoading: false,
+          weather: normalize(weather.list)
+        };
+        expect(
+          reducer(initialState, {
+            type: types.FETCH_WEATHER_SUCCESS,
+            payload: weather
+          })
+        ).toEqual(expected);
+      });
     });
   });
 });
--- src/module.js
+++ src/module.js
@@ -1,11 +1,48 @@
 import api from './api';
+import { BASE_ICON_URL } from './config';
+import { groupBy, mode } from './utils';
+
+//
+// Group the hourly weather forecasts by day to facilitate UI
+//
+export const normalize = hourly => {
+  const keyFn = value => value.dt_txt.split(' ')[0];
+  const daily = groupBy(keyFn, hourly);
+  const result = {};
+  Object.keys(daily).forEach(key => {
+    const value = daily[key];
+    result[key] = {
+      date: new Date(key),
+      tempMax: Math.round(
+        Math.max(...value.data.map(data => data.main.temp_max))
+      ),
+      tempMin: Math.round(
+        Math.min(...value.data.map(data => data.main.temp_min))
+      ),
+      icon: `${BASE_ICON_URL}/${mode(
+        value.data.map(data => data.weather[0].icon)
+      )}.png`,
+      hourly: value.data.map(data => ({
+        date: new Date(data.dt_txt),
+        temp: Math.round(data.main.temp),
+        icon: `${BASE_ICON_URL}/${data.weather[0].icon}.png`
+      }))
+    };
+  });
+  return result;
+};
 
 export const FETCH_CITIES_REQUEST = 'FETCH_CITIES_REQUEST';
 export const FETCH_CITIES_SUCCESS = 'FETCH_CITIES_SUCCESS';
 
+export const FETCH_WEATHER_REQUEST = 'FETCH_WEATHER_REQUEST';
+export const FETCH_WEATHER_SUCCESS = 'FETCH_WEATHER_SUCCESS';
+
 export const types = {
   FETCH_CITIES_REQUEST,
-  FETCH_CITIES_SUCCESS
+  FETCH_CITIES_SUCCESS,
+  FETCH_WEATHER_REQUEST,
+  FETCH_WEATHER_SUCCESS
 };
 
 export const fetchCitiesRequest = () => ({ type: FETCH_CITIES_REQUEST });
@@ -19,10 +56,24 @@ export const fetchCities = filter => async dispatch => {
   dispatch(fetchCitiesSuccess(data));
 };
 
+export const fetchWeatherRequest = () => ({ type: FETCH_WEATHER_REQUEST });
+export const fetchWeatherSuccess = payload => ({
+  type: FETCH_WEATHER_SUCCESS,
+  payload
+});
+export const fetchWeather = cityId => async dispatch => {
+  dispatch(fetchWeatherRequest());
+  const data = await api.getHourlyForecast(cityId);
+  dispatch(fetchWeatherSuccess(data));
+};
+
 export const actions = {
   fetchCitiesRequest,
   fetchCitiesSuccess,
-  fetchCities
+  fetchCities,
+  fetchWeatherRequest,
+  fetchWeatherSuccess,
+  fetchWeather
 };
 
 const ACTION_HANDLERS = {
@@ -31,11 +82,18 @@ const ACTION_HANDLERS = {
     ...state,
     cities: action.payload.map(city => ({ value: city.id, text: city.name })),
     isLoading: false
+  }),
+  [FETCH_WEATHER_REQUEST]: state => ({ ...state, isLoading: true }),
+  [FETCH_WEATHER_SUCCESS]: (state, action) => ({
+    ...state,
+    weather: normalize(action.payload.list),
+    isLoading: false
   })
 };
 
 export const initialState = {
   cities: [],
+  weather: {},
   isLoading: false
 };
 
